1a7ee7eeace158b15ff97382aa70d9cea22c89bb
MathPerson12321 <mathguy12321@gmail.com>
2025-01-11 09:33:01 -0700

    a

--- src/main/java/frc/robot/Subsystems/Algae/AlgaeIoSim.java
+++ src/main/java/frc/robot/Subsystems/Algae/AlgaeIoSim.java
@@ -1,7 +1,6 @@
 package frc.robot.Subsystems.Algae;
 
 import frc.helpers.CCMotorController;
-import frc.robot.Subsystems.Algae.AlgaeIOSim;
 
 public class AlgaeIOSim implements AlgaeIO {
   public AlgaeIOSim(CCMotorController wrist, CCMotorController intake) {}
