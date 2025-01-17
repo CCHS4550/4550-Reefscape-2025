6ea5457c4a786e36a3d6f03c8134be01a3f9e6ff
Suhit <111322676+suhit-m@users.noreply.github.com>
2025-01-16 20:51:17 -0700

    Delete src/main/java directory

--- src/main/java/frc/robot/Subsystems/Algae/AlgaeIoSim.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.algae;
-
-import frc.helpers.CCMotorController;
-
-public class AlgaeIOSim implements AlgaeIO {
-  public AlgaeIOSim(CCMotorController wrist, CCMotorController intake) {}
-}
